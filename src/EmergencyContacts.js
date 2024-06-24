1ccd7d4ac697b0ca4fe9c5178d3f23d65700dd6e
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-06-24 23:02:22 +0300

    updated emergency contacts

--- src/EmergencyContacts.js
+++ src/EmergencyContacts.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './EmergencyContacts.css'; // Import the CSS file for EmergencyContacts
+import './EmergencyContacts.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import ambimage from './amb.JPG';
 
 const contacts = [
     { type: 'Organisation', name: 'Defenders Coalition', phone: '0716-200100' },
     { type: 'Organisation', name: 'IMLU Org', phone: '0800 721 401' },
     { type: 'Organisation', name: 'Law Society Of Kenya', phone: '0800 720 434' },
     { type: 'Organisation', name: 'Medics For Kenya', phone: '0708311740 , 0739567483' },
+    { type: 'Organisation', name: 'KNHCR', phone: '0800 720 627' },
+    { type: 'Organisation', name: 'HakiKNCHR', phone: '22359', additionalInfo: 'SMS' },
+    { type: 'Organisation', name: 'KNHRC', phone: '0798 849 871', additionalInfo: 'WhatsApp' },
+    { type: 'Organisation', name: 'Therapists For Kenya', phone: '0715709737' },
 ];
 
 const EmergencyContacts = () => {
@@ -15,9 +22,17 @@ const EmergencyContacts = () => {
             <ul>
                 {contacts.map((contact, index) => (
                     <li key={index}>
-                        <strong>{contact.type}:</strong> {contact.name} - <a href={`tel:${contact.phone}`}>{contact.phone}</a>
+                        <strong>{contact.type}:</strong> {contact.name} - 
+                        <a href={`tel:${contact.phone.replace(/\s*,\s*/g, '|tel:')}`}>{contact.phone}</a>
+                        {contact.additionalInfo && <span> ({contact.additionalInfo})</span>}
                     </li>
                 ))}
+                <li>
+                    <strong>Emergency Ambulance List:</strong> 
+                    <a href={ambimage} download="Emergency_Ambulance_Contacts.JPG">
+                        <FontAwesomeIcon icon={faDownload} /> Download
+                    </a>
+                </li>
             </ul>
         </div>
     );
